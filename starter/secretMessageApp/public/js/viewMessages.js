67da596893453b947abaafec3d1bb886762db162
Eric Choi <ericchoirr38@gmail.com>
2021-07-22 17:46:15 +0000

    day 9 independent practice with spicy

--- starter/secretMessageApp/public/js/viewMessages.js
+++ starter/secretMessageApp/public/js/viewMessages.js
@@ -1,3 +1,26 @@
+document.querySelector("#viewMsg").addEventListener("click", (e)=>{
+    const userPasscodeGuess = document.querySelector("#passcode").value;
+
+    const messagesRef = firebase.database().ref();
+    messagesRef.on("value", (snapshot) =>{
+        const data = snapshot.val();
+        for(let key in data){
+            if(data[key].passcode === userPasscodeGuess){
+                display(data[key]);
+            }
+        }
+    });
+});
+
+function display(message){
+    //console.log(message);
+    const messageText = document.querySelector("#message");
+    const formPortion = document.querySelector("#passcodeInput");
+    messageText.innerHTML = message.message;
+    formPortion.style.display = "none";
+}
+
+/*
 const getMessages = () => {
     const messagesRef = firebase.database().ref();
     messagesRef.on('value', (snapshot) => {
@@ -30,3 +53,4 @@ document.querySelector("#viewMsg").addEventListener("click", () => {
     const passcode = document.querySelector("#passcode").value;
     findMessage(passcode);
 });
+*/
---
+++ starter/secretMessageApp/public/js/writeMessage.js
@@ -0,0 +1,10 @@
+const messagesRef = firebase.database().ref();
+const inputPass = document.querySelector("#passcode");
+const inputText = document.querySelector("#message");
+
+document.querySelector("#")
+messagesRef.push({
+    message: "This was made with JS",
+    passcode:"JS"
+});
+
